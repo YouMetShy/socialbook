806df7bb9b1fff3cb7efe5659a639b097d568915
Your Name <you@example.com>
2023-02-08 11:30:17 -0400

    first commit

---
+++ client/main.js
@@ -0,0 +1,8 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import * as bootstrap from 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import './main.html';
+import '../lib/collections.js'
+import '../imports/layout.js'
